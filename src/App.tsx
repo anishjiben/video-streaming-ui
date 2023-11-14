94443e1f2af24f6c8401dfac5ae6ec34e62c031d
ajiben <anish.jiben@wescodist.com>
2023-11-14 09:21:24 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: "100%" }}>
+      <div style={{ width: 650, margin: "auto" }}>
+        <span style={{ margin: "auto", fontSize: "2em" }}>Video Streaming</span>
+        <ReactPlayer
+          muted={true}
+          controls
+          url="http://localhost:8000/manifest.mpd"
+          config={{
+            file: {
+              attributes: {
+                preload: "none",
+              },
+            },
+          }}
+        />
+        {/* <video id="videoPlayer" width="650" controls autoPlay muted>
+          <source src="http://localhost:8000/video#t=0" type="video/mp4" />
+        </video> */}
+      </div>
     </div>
   );
 }
