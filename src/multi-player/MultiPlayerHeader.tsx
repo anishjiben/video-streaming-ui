cdc75770c65c483d9f610f8e90c95266c6a71717
Anish Jiben <anishjiben@gmail.com>
2023-11-23 17:25:46 +0530

    Multi player

--- src/multi-player/MultiPlayerHeader.tsx
+++ src/multi-player/MultiPlayerHeader.tsx
@@ -2,7 +2,12 @@ import WescoLogo from "@WESCO-International/wdp-ui-components/components/icon/We
 import { Dropdown } from "@WESCO-International/wdp-ui-components/components/dropdown";
 import { useState } from "react";
 
-export const MultiPlayerHeader = () => {
+type MultiPlayerHeaderProps = {
+  onMatrixSelected: (matrix: string) => void;
+};
+export const MultiPlayerHeader = ({
+  onMatrixSelected,
+}: MultiPlayerHeaderProps) => {
   const [rowsCols, setRowsCols] = useState("1 X 1");
   const rowsAndColsOptins = [
     { label: "2 X 2", value: "2X2" },
@@ -14,7 +19,10 @@ export const MultiPlayerHeader = () => {
       <Dropdown
         value={rowsCols}
         emptyLabel="Select Grid"
-        onChange={(o) => setRowsCols(o.value)}
+        onChange={(o) => {
+          setRowsCols(o.value);
+          onMatrixSelected(o.value);
+        }}
         options={rowsAndColsOptins}
         style={{
           width: "200px",
--- src/multi-player/MultiplePlayer.tsx
+++ src/multi-player/MultiplePlayer.tsx
@@ -5,20 +5,20 @@ import MultiPlayerHeader from "./MultiPlayerHeader";
 export const MultiplePlayer = () => {
   const cameraNames = ["Camera 1", "Camera 2", "Camera 3", "Camera 4"];
   const [selectedCamera, setSelectedCamera] = useState<string>("");
-
+  const [noOfRows, setnoOfRows] = useState<any>(1);
   const createCameraGrid = () => {
     const gridContainer = document.getElementById("grid-container");
     if (gridContainer?.childElementCount) {
       gridContainer.replaceChildren();
     }
 
-    for (let row = 0; row < 3; row++) {
+    for (let row = 0; row < noOfRows; row++) {
       const rowElement = document.createElement("div");
       rowElement.classList.add("cols", "m-0");
 
-      for (let col = 0; col < 3; col++) {
+      for (let col = 0; col < noOfRows; col++) {
         const cellElement = document.createElement("div");
-        cellElement.classList.add("col", "m-1", "border");
+        cellElement.classList.add("col", "m-1", "border", "bg-primary-light");
 
         cellElement.textContent = `Row ${row + 1}, Column ${col + 1}`;
         rowElement.appendChild(cellElement);
@@ -31,17 +31,30 @@ export const MultiplePlayer = () => {
   useEffect(() => {
     createCameraGrid();
   }, []);
+  useEffect(() => {
+    if (noOfRows) {
+      console.log(noOfRows);
+      createCameraGrid();
+    }
+  }, [noOfRows]);
 
   return (
     <div className="h-100 w-100">
       <div className="border" style={{ height: "10%" }}>
-        <MultiPlayerHeader />
+        <MultiPlayerHeader
+          onMatrixSelected={(selectedMatrix) => {
+            setnoOfRows(+selectedMatrix.charAt(0));
+          }}
+        />
       </div>
       <div className="flex" style={{ height: "90%" }}>
         <div className="br" style={{ width: "20%" }}>
           <CameraList
             cameraNames={cameraNames}
-            onCameraSelect={setSelectedCamera}
+            onCameraSelect={(cameraName: string) => {
+              setnoOfRows(1);
+              setSelectedCamera(cameraName);
+            }}
           />
         </div>
         <div
@@ -49,7 +62,7 @@ export const MultiplePlayer = () => {
           style={{
             width: "80%",
             display: "grid",
-            gridTemplateRows: "repeat(3, 1fr)",
+            gridTemplateRows: `repeat(${noOfRows}, 1fr)`,
           }}
         ></div>
       </div>
