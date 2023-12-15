02f3d991a501ff934bd7549f704f64221cf5f308
anishjiben@gmail.com <anishjiben@gmail.com>
2023-12-15 09:48:49 +0530

    websocket

--- src/mobile-app-component/MobileAppComponent.tsx
+++ src/mobile-app-component/MobileAppComponent.tsx
@@ -5,71 +5,102 @@ import { Button } from "@WESCO-International/wdp-ui-components";
 
 const WSurl = "http://localhost:8000";
 
+const constraints = {
+  video: true,
+  audio: false,
+};
+const getUserMedia = async () => {
+  return await navigator.mediaDevices.getUserMedia(constraints);
+};
 export const MobileAppComponent = () => {
   const socket = socketClient(WSurl);
   const [me, setMe] = useState<string>("");
+  const videoRef = useRef<any>();
+  const [initater, setInitiater] = useState<any>();
+  const [peerConnection, setPeerConnection] = useState<RTCPeerConnection>();
   const [stream, setStream] = useState<any>();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState<any>();
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [idToCall, setIdToCall] = useState("");
-  const [callEnded, setCallEnded] = useState(false);
-  const [name, setName] = useState("");
-  //   const myVideo = useRef<any>();
-  const mobileVideo = useRef<any>();
-  const connectionRef = useRef<any>();
 
   useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        setStream(stream);
-        mobileVideo.current.srcObject = stream;
-      });
-
-    socket.on("me", (id) => {
-      console.log("Websocket id : ", id);
+    socket.on("me", async (id) => {
       setMe(id);
+      console.log(id);
+      const configuration = {
+        iceServers: [
+          {
+            urls: "stun:20.25.113.232:3478",
+          },
+        ],
+      };
+      const peerConnection: RTCPeerConnection = new RTCPeerConnection(
+        configuration
+      );
+
+      // ICE Candidate
+      peerConnection.addEventListener(
+        "connectionstatechange",
+        async (event) => {
+          if (peerConnection.connectionState === "connected") {
+            console.log("connection status : ", peerConnection.connectionState);
+            const localStream: any = await getUserMedia();
+            videoRef.current.srcObject = localStream;
+            peerConnection.addTrack(localStream.getTracks()[0]);
+            // Peers connected!
+          }
+        }
+      );
+
+      socket.on("message", async (data: any) => {
+        console.log(data);
+        if (data.type === "offer") {
+          peerConnection.setRemoteDescription(
+            new RTCSessionDescription(data.offer)
+          );
+          const answer = await peerConnection.createAnswer();
+          await peerConnection.setLocalDescription(answer);
+          socket.emit("message", {
+            from: id,
+            to: data.from,
+            type: "answer",
+            answer: answer,
+          });
+        } else if (data.type === "new-ice-candidate") {
+          await peerConnection.addIceCandidate(data.iceCandidate);
+        }
+        setInitiater(data);
+        peerConnection.addEventListener("icecandidate", (event) => {
+          // console.log("connection status : ", event);
+          if (event.candidate) {
+            socket.emit("message", {
+              from: id,
+              to: data.from,
+              type: "new-ice-candidate",
+              iceCandidate: event.candidate,
+            });
+          }
+        });
+      });
+      setPeerConnection(peerConnection);
     });
 
-    socket.on("callUser", (data) => {
-      console.log("request stream");
-      setReceivingCall(true);
-      setCaller(data.from);
-      setName(data.name);
-      setCallerSignal(data.signal);
-    });
+    return function cleanup() {
+      // socket.disconnect();
+      socket.removeAllListeners();
+    };
   }, []);
 
-  const answerCall = () => {
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-    peer.signal(callerSignal);
-
-    // peer.addStream(stream);
-    // peer.emit("stream", stream);
-    peer.on("signal", (data) => {
-      if (data.type === "answer") {
-        console.log("answer ", data, caller);
-        socket.emit("answerCall", { signal: data, to: caller });
-        setReceivingCall(false);
-      }
-    });
-    // peer.on("stream", (stream) => {
-    //   mobileVideo.current.srcObject = stream;
-    // });
-
-    connectionRef.current = peer;
+  const replyToPing = async () => {
+    // if (peerConnection) {
+    //   const localStream = await getUserMedia();
+    //   console.log(localStream.getTracks());
+    //   localStream.getTracks().forEach((track: any) => {
+    //     peerConnection.addTrack(track, localStream);
+    //   });
+    // }
   };
-
   return (
     <div>
-      <Button label="Stream" onClick={answerCall} disabled={!receivingCall} />
-      <video id="webrtc-player-mobile" ref={mobileVideo} autoPlay />
+      <Button label="Stream" onClick={replyToPing} disabled={!initater} />
+      <video id="webrtc-player-mobile" ref={videoRef} autoPlay />
     </div>
   );
 };
--- src/webrtc-player/WebRtcPlayer.tsx
+++ src/webrtc-player/WebRtcPlayer.tsx
@@ -6,63 +6,105 @@ import { Input } from "@WESCO-International/wdp-ui-components/components/input";
 
 const WSurl = "http://localhost:8000";
 
+const constraints = {
+  video: true,
+  audio: false,
+};
+const getUserMedia = async () => {
+  return await navigator.mediaDevices.getUserMedia(constraints);
+};
 export const WebRtcPlayer = () => {
   const socket = socketClient(WSurl);
   const [me, setMe] = useState<string>("");
-  const [stream, setStream] = useState<any>();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState<any>();
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [idToCall, setIdToCall] = useState("");
-  const [callEnded, setCallEnded] = useState(false);
-  const [name, setName] = useState("");
-  //   const myVideo = useRef<any>();
-  const videoPlayer = useRef<any>();
-  const connectionRef = useRef<any>();
+  const videoRef = useRef<any>();
+  const [idToCall, setIdToCall] = useState<string>();
+  const [peerConnection, setPeerConnection] = useState<RTCPeerConnection>();
+  const [offer, setOffer] = useState<any>();
+
+  const configuration = {
+    iceServers: [
+      {
+        urls: "stun:20.25.113.232:3478",
+      },
+    ],
+  };
 
   useEffect(() => {
-    socket.on("me", (id) => {
+    socket.on("me", async (id) => {
       setMe(id);
-      console.log("socket id : ", id);
+      const peerConnection = new RTCPeerConnection(configuration);
+      const offer = await peerConnection.createOffer({
+        offerToReceiveAudio: true,
+        offerToReceiveVideo: true,
+      });
+      await peerConnection.setLocalDescription(offer);
+      setOffer(offer);
+
+      peerConnection.addEventListener(
+        "connectionstatechange",
+        async (event) => {
+          console.log("connection status : ", peerConnection.connectionState);
+          if (peerConnection.connectionState === "connected") {
+            // peerConnection.createDataChannel("video-channel");
+            // const localStream: any = await getUserMedia();
+            // videoRef.current.srcObject = localStream;
+            // Peers connected!
+          }
+        }
+      );
+      peerConnection.addEventListener("icecandidate", async (event) => {
+        if (event.candidate) {
+          socket.emit("message", {
+            from: id,
+            to: idToCall,
+            type: "new-ice-candidate",
+            iceCandidate: event.candidate,
+          });
+        }
+      });
+      peerConnection.addEventListener("track", (event) => {
+        console.log("Track received : ", event);
+        const [remoteStream] = event.streams;
+        videoRef.current.srcObject = remoteStream;
+      });
+      socket.on("message", async (data: any) => {
+        console.log(data);
+        if (data.type === "answer") {
+          const remoteDesc = new RTCSessionDescription(data.answer);
+          await peerConnection.setRemoteDescription(remoteDesc);
+        } else if (data.type === "new-ice-candidate") {
+          await peerConnection.addIceCandidate(data.iceCandidate);
+        }
+      });
+
+      setPeerConnection(peerConnection);
     });
+    return function cleanup() {
+      socket.removeAllListeners();
+    };
   }, []);
 
-  const callUser = () => {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      //   stream: stream,
-    });
-    peer.on("signal", (data) => {
-      socket.emit("callUser", {
-        userToCall: idToCall,
-        signalData: data,
+  const pingStreamerDevice = async () => {
+    if (peerConnection) {
+      socket.emit("message", {
         from: me,
-        name: name,
+        to: idToCall,
+        type: "offer",
+        offer: offer,
       });
-    });
-    peer.on("stream", (stream) => {
-      videoPlayer.current.srcObject = stream;
-    });
-    socket.on("offerAccepted", (targetSignal: any) => {
-      console.log("Websocket : ", targetSignal);
-      setCallAccepted(true);
-      peer.signal(targetSignal);
-    });
-    connectionRef.current = peer;
+    }
   };
 
   return (
     <div>
-      <Button label="Request Stream" onClick={callUser} />
+      <Button label="Request Stream" onClick={pingStreamerDevice} />
       <Input
         id="filled-basic"
         label="ID to call"
         value={idToCall}
         onChange={(e) => setIdToCall(e.target.value)}
       />
-      <video id="webrtc-player-web" ref={videoPlayer} autoPlay />
+      <video id="webrtc-player-web" ref={videoRef} autoPlay />
     </div>
   );
 };
